a15fabc2012c9b20853308ecc89084f998171042
augusito <kaugustus15@gmail.com>
2022-10-18 21:30:37 +0300

    docs(): add rfc to  each status code

--- src/status-code.enum.ts
+++ src/status-code.enum.ts
@@ -2,70 +2,128 @@
  * Enumeration of HTTP status codes.
  */
 export enum StatusCode {
-  // 1xx Informational
+  /** RFC 7231, 6.2.1 */
   CONTINUE = 100,
+  /** RFC 7231, 6.2.2 */
   SWITCHING_PROTOCOLS = 101,
+  /** RFC 2518, 10.1 */
   PROCESSING = 102,
+  /** RFC 8297 **/
   EARLYHINTS = 103,
-  // 2xx Success
+
+  /** RFC 7231, 6.3.1 */
   OK = 200,
+  /** RFC 7231, 6.3.2 */
   CREATED = 201,
+  /** RFC 7231, 6.3.3 */
   ACCEPTED = 202,
+  /** RFC 7231, 6.3.4 */
   NON_AUTHORITATIVE_INFORMATION = 203,
+  /** RFC 7231, 6.3.5 */
   NO_CONTENT = 204,
+  /** RFC 7231, 6.3.6 */
   RESET_CONTENT = 205,
+  /** RFC 7233, 4.1 */
   PARTIAL_CONTENT = 206,
+  /** RFC 4918, 11.1 */
   MULTI_STATUS = 207,
+  /** RFC 5842, 7.1 */
   ALREADY_REPORTED = 208,
+  /** RFC 3229, 10.4.1 */
   IM_USED = 226,
-  // 3xx Redirection
+
+  /** RFC 7231, 6.4.1 */
   MULTIPLE_CHOICES = 300,
+  /** RFC 7231, 6.4.2 */
   MOVED_PERMANENTLY = 301,
+  /** RFC 7231, 6.4.3 */
   FOUND = 302,
+  /** RFC 7231, 6.4.4 */
   SEE_OTHER = 303,
+  /** RFC 7232, 4.1 */
   NOT_MODIFIED = 304,
+  /** RFC 7231, 6.4.5 */
   USE_PROXY = 305,
+  /** RFC 7231, 6.4.7 */
   TEMPORARY_REDIRECT = 307,
+  /** RFC 7538, 3 */
   PERMANENT_REDIRECT = 308,
-  // 4xx Client Error
+
+  /** RFC 7231, 6.5.1 */
   BAD_REQUEST = 400,
+  /** RFC 7235, 3.1 */
   UNAUTHORIZED = 401,
+  /** RFC 7231, 6.5.2 */
   PAYMENT_REQUIRED = 402,
+  /** RFC 7231, 6.5.3 */
   FORBIDDEN = 403,
+  /** RFC 7231, 6.5.4 */
   NOT_FOUND = 404,
+  /** RFC 7231, 6.5.5 */
   METHOD_NOT_ALLOWED = 405,
+  /** RFC 7231, 6.5.6 */
   NOT_ACCEPTABLE = 406,
+  /** RFC 7235, 3.2 */
   PROXY_AUTHENTICATION_REQUIRED = 407,
+  /** RFC 7231, 6.5.7 */
   REQUEST_TIMEOUT = 408,
+  /** RFC 7231, 6.5.8 */
   CONFLICT = 409,
+  /** RFC 7231, 6.5.9 */
   GONE = 410,
+  /** RFC 7231, 6.5.10 */
   LENGTH_REQUIRED = 411,
+  /** RFC 7232, 4.2 */
   PRECONDITION_FAILED = 412,
+  /** RFC 7231, 6.5.11 */
   PAYLOAD_TOO_LARGE = 413,
+  /** RFC 7231, 6.5.12 */
   URI_TOO_LONG = 414,
+  /** RFC 7231, 6.5.13 */
   UNSUPPORTED_MEDIA_TYPE = 415,
+  /** RFC 7233, 4.4 */
   REQUESTED_RANGE_NOT_SATISFIABLE = 416,
+  /** RFC 7231, 6.5.14 */
   EXPECTATION_FAILED = 417,
+  /** RFC 7168, 2.3.3 */
   I_AM_A_TEAPOT = 418,
+  /** RFC 7540, 9.1.2 */
   MISDIRECTED = 421,
+  /** RFC 4918, 11.2 */
   UNPROCESSABLE_ENTITY = 422,
+  /** RFC 4918, 11.3 */
   LOCKED = 423,
+  /** RFC 4918, 11.4 */
   FAILED_DEPENDENCY = 424,
+  /** RFC 6585, 3 */
   PRECONDITION_REQUIRED = 428,
+  /** RFC 6585, 4 */
   TOO_MANY_REQUESTS = 429,
+  /** RFC 6585, 5 */
   REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
+  /** RFC 7725, 3 */
   UNAVAILABLE_FOR_LEGAL_REASONS = 451,
-  // 5xx Server Error
+
+  /** RFC 7231, 6.6.1 */
   INTERNAL_SERVER_ERROR = 500,
+  /** RFC 7231, 6.6.2 */
   NOT_IMPLEMENTED = 501,
+  /** RFC 7231, 6.6.3 */
   BAD_GATEWAY = 502,
+  /** RFC 7231, 6.6.4 */
   SERVICE_UNAVAILABLE = 503,
+  /** RFC 7231, 6.6.5 */
   GATEWAY_TIMEOUT = 504,
+  /** RFC 7231, 6.6.6 */
   HTTP_VERSION_NOT_SUPPORTED = 505,
+  /** RFC 2295, 8.1 */
   VARIANT_ALSO_NEGOTIATES = 506,
+  /** RFC 4918, 11.5 */
   INSUFFICIENT_STORAGE = 507,
+  /** RFC 5842, 7.2 */
   LOOP_DETECTED = 508,
-  BANDWIDTH_LIMIT_EXCEEDED = 509,
+  /** RFC 2774, 7 */
   NOT_EXTENDED = 510,
+  /** RFC 6585, 6 */
   NETWORK_AUTHENTICATION_REQUIRED = 511,
 }
